98fe481a2627e8c72f424e056551e031bcba8d12
Shunchao Wang <wang.2799@osu.edu>
2018-11-29 09:56:32 -0500

    Removed junit.

--- core/src/test/java/boot/security/core/LibraryTest.java
+++ core/src/test/java/boot/security/core/LibraryTest.java
@@ -3,12 +3,6 @@
  */
 package boot.security.core;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 public class LibraryTest {
-    @Test public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
-    }
+
 }
