3cfa79c7ef519f7363a64de687072404b3f7e6ae
Shunchao Wang <wang.2799@osu.edu>
2018-11-30 14:13:17 -0500

    Working on user controller.

--- demo/src/main/java/boot/security/demo/web/UserController.java
+++ demo/src/main/java/boot/security/demo/web/UserController.java
@@ -8,7 +8,9 @@ import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,13 +22,21 @@ public class UserController {
 
   @GetMapping()
 //  public List<User> query(@RequestParam String username) {
-  public List<User> query(UserQueryCriteria criteria) {
+  public List<User> query(UserQueryCriteria criteria, Pageable pageable) {
 
     logger.info(ReflectionToStringBuilder.toString(criteria, ToStringStyle.MULTI_LINE_STYLE));
+    logger.info(ReflectionToStringBuilder.toString(pageable, ToStringStyle.MULTI_LINE_STYLE));
     List<User> users = new ArrayList<>();
     for (int i = 0; i < 3; i++) {
       users.add(new User());
     }
     return users;
+  }
+
+  @GetMapping("/{id:\\d+}")
+  public User getInfo(@PathVariable String id) {
+    User user = new User();
+    user.setUsername("tom");
+    return user;
   }
 }
