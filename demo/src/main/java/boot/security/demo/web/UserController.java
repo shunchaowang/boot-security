1226c6316d4f5fe4b094b0dfe47efaf6f24b943d
Shunchao Wang <wang.2799@osu.edu>
2018-12-05 20:59:22 -0500

    Error pages and ControllerAdvice to handle different exceptions.

--- demo/src/main/java/boot/security/demo/web/UserController.java
+++ demo/src/main/java/boot/security/demo/web/UserController.java
@@ -1,6 +1,5 @@
 package boot.security.demo.web;
 
-import boot.security.demo.UserNotExistException;
 import boot.security.demo.model.User;
 import boot.security.demo.model.UserQueryCriteria;
 import com.fasterxml.jackson.annotation.JsonView;
@@ -46,10 +45,10 @@ public class UserController {
   @GetMapping("/{id:\\d+}")
   @JsonView(User.UserDetailView.class)
   public User getInfo(@PathVariable String id) {
-    throw new UserNotExistException(id);
-//    User user = new User();
-//    user.setUsername("tom");
-//    return user;
+//    throw new UserNotExistException(id);
+    User user = new User();
+    user.setUsername("tom");
+    return user;
   }
 
   @PostMapping
