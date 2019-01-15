86cdb0c325336aefd40ee6e668db587ee247abaf
Shunchao Wang <wang.2799@osu.edu>
2019-01-15 15:54:54 -0500

    Added.

--- app/src/main/java/boot/security/app/AppSecurityConfig.java
+++ app/src/main/java/boot/security/app/AppSecurityConfig.java
@@ -1,6 +1,9 @@
 package boot.security.app;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -22,5 +25,25 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.httpBasic();
+  }
+
+  /**
+   * Override this method to expose the {@link AuthenticationManager} from {@link
+   * #configure(AuthenticationManagerBuilder)} to be exposed as a Bean. For example:
+   *
+   * <pre>
+   * &#064;Bean(name name="myAuthenticationManager")
+   * &#064;Override
+   * public AuthenticationManager authenticationManagerBean() throws Exception {
+   *     return super.authenticationManagerBean();
+   * }
+   * </pre>
+   *
+   * @return the {@link AuthenticationManager}
+   */
+  @Override
+  @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
   }
 }
--- app/src/main/java/boot/security/app/BootAuthorizationServerConfig.java
+++ app/src/main/java/boot/security/app/BootAuthorizationServerConfig.java
@@ -1,8 +1,21 @@
 package boot.security.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
-public class BootAuthorizationServerConfig {}
+public class BootAuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+  @Autowired
+  private AuthenticationManager authenticationManager;
+
+  @Override
+  public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+    super.configure(endpoints);
+  }
+}
